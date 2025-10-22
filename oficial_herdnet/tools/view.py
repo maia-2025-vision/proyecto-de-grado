be660d6d813cb20169cea19b833379161acdccb2
aleapoveda <aleapoveda@hotmail.com>
2025-10-21 19:53:27 -0500

    fix: ajustes formateo con pre-commit e instalacion de otros paquetes por efecto del merge

--- oficial_herdnet/tools/view.py
+++ oficial_herdnet/tools/view.py
@@ -1,11 +1,11 @@
-__copyright__ = \
-    """
+__copyright__ = """
     Copyright (C) 2024 University of Liège, Gembloux Agro-Bio Tech, Forest Is Life
     All rights reserved.
 
     This source code is under the MIT License.
 
-    Please contact the author Alexandre Delplanque (alexandre.delplanque@uliege.be) for any questions.
+    Please contact the author Alexandre Delplanque (alexandre.delplanque@uliege.be)
+    for any questions.
 
     Last modification: March 18, 2024
     """
@@ -17,41 +17,47 @@ __version__ = "0.2.1"
 import argparse
 import os
 
+# from typing import List  # CHANGE BY aalea unused
+# UP035 `typing.List` is deprecated, use `list` instead
 import fiftyone as fo
 import pandas as pd
 import PIL.Image as Image
 
-from typing import List
+parser = argparse.ArgumentParser(
+    prog="view", description="View ground truth or detections (points) on images using FiftyOne app"
+)
 
-parser = argparse.ArgumentParser(prog='view', 
-    description='View ground truth or detections (points) on images using FiftyOne app')
-
-parser.add_argument('root', type=str,
-    help='path to the images directory (str)')
-parser.add_argument('gt', type=str,
-    help='path to a csv file containing points ground truth (str)')
-parser.add_argument('-dets', type=str,
-    help='path to a csv file containing a model\'s points detections (str). Defaults to None')
+parser.add_argument("root", type=str, help="path to the images directory (str)")
+parser.add_argument("gt", type=str, help="path to a csv file containing points ground truth (str)")
+parser.add_argument(
+    "-dets",
+    type=str,
+    help="path to a csv file containing a model's points detections (str). Defaults to None",
+)
 
 args = parser.parse_args()
+
 
 def _create_dataset():
     return fo.Dataset.from_images_dir(args.root)
 
+
 def _get_points_and_labels(img_path: str, df: pd.DataFrame) -> list:
     w, h = Image.open(img_path).size
     img_name = os.path.basename(img_path)
-    records = df[df['images'] == img_name].to_dict('records')
-    points = [(r['x'] / w, r['y'] / h) for r in records]
-    labels = [r['labels'] for r in records]
+    records = df[df["images"] == img_name].to_dict("records")
+    points = [(r["x"] / w, r["y"] / h) for r in records]
+    labels = [r["labels"] for r in records]
     return points, labels
 
-def _create_keypoints(points: list, labels: list) -> List[fo.Keypoint]:
+
+def _create_keypoints(points: list, labels: list) -> list[fo.Keypoint]:
     keypoints = []
-    for pt, lab in zip(points, labels):
+    for pt, lab in zip(points, labels, strict=False):
         kp = fo.Keypoint(label=str(lab), points=[pt])
         keypoints.append(kp)
     return keypoints
+
 
 def main():
     gt = pd.read_csv(args.gt)
@@ -61,18 +67,20 @@ def main():
 
     dataset = _create_dataset()
     for sample in dataset:
-
         gt_points, gt_labels = _get_points_and_labels(sample.filepath, df=gt)
-        sample['gt'] = fo.Keypoints(keypoints=_create_keypoints(gt_points, gt_labels))
+        sample["gt"] = fo.Keypoints(keypoints=_create_keypoints(gt_points, gt_labels))
 
         if dets is not None:
             dets_points, dets_labels = _get_points_and_labels(sample.filepath, df=dets)
-            sample['predictions'] = fo.Keypoints(keypoints=_create_keypoints(dets_points, dets_labels))
+            sample["predictions"] = fo.Keypoints(
+                keypoints=_create_keypoints(dets_points, dets_labels)
+            )
 
         sample.save()
-    
+
     session = fo.launch_app(dataset, port=5151)
     session.wait()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
