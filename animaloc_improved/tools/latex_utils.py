df76e8254190744de237dacfc249aed20a282509
aleapoveda <aleapoveda@hotmail.com>
2025-11-22 19:25:39 -0500

    merge from main

---
+++ animaloc_improved/tools/latex_utils.py
@@ -0,0 +1,103 @@
+from typing import Literal
+
+import pandas as pd
+
+# Model latex code
+r"""
+\begin{table}[h!]
+%% increase table row spacing, adjust to taste
+\renewcommand{\arraystretch}{1.3}
+% if using array.sty, it might be a good idea to tweak the value of
+% \extrarowheight as needed to properly center the text within the cells
+\centering
+%% Some packages, such as MDW tools, offer better commands for making tables
+%% than the plain LaTeX2e tabular which is used here.
+\begin{tabular}{c c}
+\hline
+\textbf{One} & \textbf{Two}\\   % col headers
+\hline
+Three & Four\\  % row
+\hline
+\end{tabular}
+\caption{An Example of a Table}
+\label{table_example}
+\end{table}
+"""
+
+
+def df_to_latex(
+    df: pd.DataFrame,
+    caption: str,
+    label: str | None = None,
+    placement: str = "h!",  # Force here, other options are t, b ....
+    bold_col_headers: bool = True,
+    row_index: bool = False,
+    bold_row_headers: bool = True,
+    array_stretch: float = 1.3,
+    centering: bool = True,
+    col_sep: Literal[" ", "|"] = " ",
+) -> str:
+    parts = []
+    parts.append(f"\\begin{{table}}[{placement}]")
+
+    if array_stretch is not None:
+        parts.append(f"\\renewcommand{{\\arraystretch}}{{{array_stretch}}}")
+
+    if centering:
+        parts.append("\\centering")
+
+    n_cols = len(df.columns)
+    col_spec = col_sep.join(["c"] * n_cols)
+
+    parts.append(f"\\begin{{tabular}}{{{col_spec}}}")
+    parts.append(r"\hline")  # top border
+    col_headers = [_make_column_header(col, bold_col_headers) for col in df.columns]
+
+    parts.append(" & ".join(col_headers) + r"\\")
+    parts.append(r"\hline")  # headers to rows separator
+
+    for row_idx, row in df.iterrows():
+        row_line_parts = []
+        if row_index:
+            if bold_row_headers:
+                row_line_parts.append(f"\\textbf{{{row_idx}}}")
+            else:
+                row_line_parts.append(str(row_idx))
+        row_line_parts.extend([str(val) for val in row.values])
+
+        parts.append(" & ".join(row_line_parts) + r"\\")  # each row
+
+    parts.append(r"\hline")  # bottom border
+    parts.append(r"\end{tabular}")
+    parts.append(f"\\caption{{{caption}}}")
+    if label is not None:  # put label after caption for proper numbering
+        parts.append(f"\\label{{{label}}}")
+    parts.append(r"\end{table}")
+
+    return "\n".join(parts)
+
+
+def _make_column_header(col_name: str, bold_col_headers: bool) -> str:
+    lines = col_name.split("\n")
+
+    def _one_line(line: str) -> str:
+        if bold_col_headers:
+            return f"\\textbf{{{line}}}"
+        else:
+            return col_name
+
+    if len(lines) == 1:  # single line most common case
+        return _one_line(col_name)
+    else:
+        latex_lines = r"\\".join(_one_line(line) for line in lines)
+        return f"\\makecell{{{latex_lines}}}"
+
+
+# %%
+def _interactive_test() -> None:
+    df = pd.DataFrame({"ColA": [1, 2, 3], "ColB": [4, 5, 6]})
+
+    print(df_to_latex(df, "tabla ejemplo"))
+
+
+# %%
---
+++ animaloc_improved/tools/measure_crowdedness_by_img.py
@@ -0,0 +1,239 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import typer
+from loguru import logger
+from mistree import GetMST  # type: ignore[import-untyped]
+
+from animaloc_improved.tools.latex_utils import df_to_latex
+
+app = typer.Typer(pretty_exceptions_show_locals=False, no_args_is_help=True)
+
+lbl_to_species = {
+    1: "Alcelaphinae",
+    2: "Buffalo",
+    3: "Kob",
+    4: "Warthog",
+    5: "Waterbuck",
+    6: "Elephant",
+}
+
+
+def _enrich_bbox_df(gt_df: pd.DataFrame) -> pd.DataFrame:
+    gt_df = gt_df.copy()
+
+    # subsets
+    #   Virunga: Archivos con nombres de como S_##_##_##_DSC#######.JPG
+    #   AED: Aerial Elephant Dataset = archivos con nombres de la forma <hexadecimal-largo>.JPG
+    gt_df["subdataset"] = np.where(
+        gt_df["images"].str.split("/").str[-1].str.contains("_"), "Virunga", "AED"
+    )
+    gt_df["order"] = gt_df["split"].map(lambda s: {"train": 0, "val": 1, "test": 2}[s])
+
+    gt_df["A(bbox)"] = (  # en pixeles cuadrados
+        (gt_df["x_max"] - gt_df["x_min"]) * (gt_df["y_max"] - gt_df["y_min"])
+    )
+    gt_df["L(bbox)"] = np.sqrt(gt_df["A(bbox)"])  # en pixeles (lineales)
+    # calcular centros de las cajas
+    gt_df["x"] = (gt_df["x_max"] + gt_df["x_min"]) * 0.5
+    gt_df["y"] = (gt_df["y_max"] + gt_df["y_min"]) * 0.5
+
+    return gt_df
+
+
+def _report_cnt_by_split_subset(gt_df: pd.DataFrame) -> None:
+    cnts_by_split_subset = (
+        gt_df.groupby(["split", "subdataset"])
+        .agg(
+            {
+                "labels": "count",
+                "images": "nunique",
+                "order": "mean",
+            }
+        )
+        .reset_index()
+        .sort_values("order")
+    )
+
+    total_labels = cnts_by_split_subset["labels"].sum()
+    total_images = cnts_by_split_subset["images"].sum()
+    cnts_by_split_subset["pct_labels"] = np.round(
+        100 * (cnts_by_split_subset["labels"] / total_labels), 1
+    )
+    cnts_by_split_subset["pct_images"] = np.round(
+        100 * (cnts_by_split_subset["images"] / total_images), 1
+    )
+
+    logger.info(f"Counts by split and subdataset:\n{cnts_by_split_subset.to_markdown()}")
+    print(
+        df_to_latex(
+            cnts_by_split_subset[
+                "split,subdataset,labels,pct_labels,images,pct_images".split(",")
+            ].rename(
+                columns={
+                    "labels": r"$\#$ anots.",
+                    "pct_labels": r"$\%$ anots.",
+                    "images": r"$\#$ imágenes",
+                    "pct_images": r"$\%$ imágenes",
+                }
+            ),
+            caption=r"Conteo de imágenes y anotaciones por \textit{split} y \textit{subdataset}",
+        )
+    )
+
+
+def _report_by_subds_lbl(gt_df: pd.DataFrame) -> None:
+    cnts_by_subds_lbl = (
+        pd.DataFrame(
+            gt_df.groupby(["subdataset", "labels"]).agg(
+                {
+                    "x_min": "count",
+                    "L(bbox)": "median",
+                }
+            )
+        )
+        .reset_index()
+        .rename(columns={"x_min": "n_annots"})
+    )
+    cnts_by_subds_lbl["L(bbox)"] = np.round(cnts_by_subds_lbl["L(bbox)"], 1)
+
+    total_annots = cnts_by_subds_lbl["n_annots"].sum()
+
+    cnts_by_subds_lbl["Especie"] = cnts_by_subds_lbl["labels"].map(lambda lbl: lbl_to_species[lbl])
+    cnts_by_subds_lbl["pct_annots"] = np.round(
+        100 * cnts_by_subds_lbl["n_annots"] / total_annots, 1
+    )
+
+    logger.info(f"All cols: {list(cnts_by_subds_lbl.columns)}")
+    cnts_by_subds_lbl = cnts_by_subds_lbl[
+        "subdataset,Especie,n_annots,pct_annots,L(bbox)".split(",")
+    ]
+
+    logger.info(f"Label counts by subdataset and species:\n{cnts_by_subds_lbl.to_markdown()}")
+    print(
+        df_to_latex(
+            cnts_by_subds_lbl.rename(
+                columns={
+                    "subdataset": "Subdataset",
+                    "n_annots": "Bboxes\n{}$\\#$",
+                    "pct_annots": "BBoxes.\n{}[\\%]",
+                    "L(bbox)": "Mediana de L(bbox)\n{}[px]",
+                }
+            ),
+            caption=r"Conteo de anotaciones por \textit{subdataset} y \textit{especie}",
+        )
+    )
+
+
+def calc_median_mis_edge(img_df: pd.DataFrame) -> float:
+    """Calcular la mediana de la longitud de las aristas del minimum spanning tree."""
+    if len(img_df) == 1:
+        return float("nan")  # solo una anotación, esto es lo menos crowded que se puede
+    else:
+        mis = GetMST(x=img_df["x"].values, y=img_df["y"].values)
+        mis.get_stats(k_neighbours=len(img_df) - 1)
+        return float(np.median(mis.edge_length))
+
+
+def _counts_by_label(img_df: pd.DataFrame) -> dict[int, int]:
+    counts_by_label_dict = img_df["labels"].value_counts().to_dict()
+    labels_mode = img_df["labels"].mode()
+    if len(labels_mode) > 1:
+        print(f"labels_mode={labels_mode.values!r}")
+    return pd.Series(  # type: ignore[return-value]
+        {
+            "dominant_species": labels_mode.iloc[0],  # resolve ties arbitrarily
+            "counts_by_label": counts_by_label_dict,
+            "has_mixed_species": len(counts_by_label_dict) > 1,
+        }
+    )
+
+
+def _produce_stats_by_image(gt_df: pd.DataFrame) -> pd.DataFrame:
+    meta_cols = ["split", "subdataset", "images"]
+    image_meta = gt_df[meta_cols].drop_duplicates()  # una fila for imagen única
+    logger.info(f"{len(image_meta)=}")
+
+    num_annots_per_image = (
+        gt_df.groupby(["images"])
+        .agg({"labels": "count", "L(bbox)": "median"})
+        .reset_index()
+        .rename(columns={"labels": "num_bboxes", "L(bbox)": "median_l_bbox"})
+    )
+
+    med_mis_edge_len = (
+        gt_df.groupby("images")
+        .apply(calc_median_mis_edge, include_groups=False)  # type: ignore[call-overload]
+        .reset_index()
+        .rename(columns={0: "median_animal_sep"})
+    )
+
+    cnts_by_label = gt_df.groupby("images").apply(
+        _counts_by_label,
+        include_groups=False,  # type: ignore[call-overload]
+    )
+
+    print(cnts_by_label, type(cnts_by_label))
+
+    stats_by_image = (
+        image_meta.merge(num_annots_per_image, on="images")
+        .merge(cnts_by_label, on="images")
+        .merge(med_mis_edge_len, on="images")
+    )
+
+    stats_by_image["normalized_animal_sep"] = (
+        stats_by_image["median_animal_sep"] / stats_by_image["median_l_bbox"]
+    )
+    stats_by_image = stats_by_image.sort_values("normalized_animal_sep")
+
+    return stats_by_image
+
+
+@app.command()
+def main(
+    gt_base_dir: Path = typer.Option(
+        Path("data/groundtruth/csv"), "--gt-dir", help="Path to ground truth csv directory"
+    ),
+    input_suffix: str = typer.Option(
+        "_big_size_A_B_E_K_WH_WB-fixed-header.csv",
+        help="suffix for bbox annotation files to be taken into account",
+    ),
+    out_dir: Path = typer.Option(..., help="output directory"),
+) -> None:
+    # gt_fnames = ["train_big_size_A_B_E_K_WH_WB-fixed-header.csv",
+    #     "val_big_size_A_B_E_K_WH_WB-fixed-header.csv",
+    #     "test_big_size_A_B_E_K_WH_WB-fixed-header.csv"]
+    gt_paths = list(gt_base_dir.glob(f"*{input_suffix}"))
+    logger.info(f"Loading ground truth annotations from {gt_paths!s}")
+    gt_parts = []
+    for gt_path in gt_paths:
+        df = pd.read_csv(gt_path)
+        df["split"] = gt_path.name.split("_")[0]
+        df["images"] = df["split"] + "/" + df["images"]
+        gt_parts.append(df)
+
+    gt_df = pd.concat(gt_parts)
+
+    gt_df = _enrich_bbox_df(gt_df)
+
+    cnts_by_split = gt_df.groupby("split").count()[["labels"]].reset_index()
+    logger.info(f"Counts by split:\n{cnts_by_split.to_markdown()}")
+
+    # cnts_by_split_subset = gt_df.groupby(["split", "subdataset"])
+    # .count()[['labels']].reset_index()
+    _report_cnt_by_split_subset(gt_df)
+    _report_by_subds_lbl(gt_df)
+
+    stats_by_image = _produce_stats_by_image(gt_df)
+
+    out_dir.mkdir(exist_ok=True, parents=True)
+    stats_by_img_path = out_dir / (input_suffix.strip("_") + ".csv")
+    logger.info(f"\n{stats_by_image.sample(50).to_markdown()}")
+
+    logger.info(f"Writing stats by image to {stats_by_img_path!s}")
+    stats_by_image.to_csv(stats_by_img_path, index=False)
+
+
+if __name__:
+    app()
